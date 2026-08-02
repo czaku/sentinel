5885ca6e9a44786a7aebf032100595c0576d94c7
Luke Czak <luke.czak@gmail.com>
2026-08-02 10:12:05 +0100

    fix: escape newlines/CR/tabs in Swift string literals (multiline values produced unterminated literals)

--- src/schema/generators/apple/strings.ts
+++ src/schema/generators/apple/strings.ts
@@ -74,4 +74,7 @@ function escapeSwiftString(value: string): string {
   return value
     .replace(/\\/g, '\\\\')
     .replace(/"/g, '\\"')
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/\t/g, '\\t')
 }
